d5cd702093b2104bf69b905dd0c3628582e50608
Rania84 <raniamoanes84@gmail.com>
2023-04-11 13:38:30 -0400

    2

--- src/test/java/Rania.java
+++ src/test/java/Rania.java
@@ -1,5 +1,9 @@
 public class Rania {
     public static void main(String[] args) {
         System.out.println(" lord help me  ");
-    }
-}
+
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(i);
+        }
+    }}
